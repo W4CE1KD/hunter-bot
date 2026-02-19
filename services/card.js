0d1728ac487b9816604eaa06e905afee37dd2ed8
John Hman <evertonbeal82@gmail.com>
2026-02-19 14:06:24 +0530

    Update card.js

--- services/card.js
+++ services/card.js
@@ -21,7 +21,7 @@ async function generateCard(user) {
   const ctx = canvas.getContext("2d");
 
   const template = await loadImage(
-    path.join(__dirname, "../assets/license-template-Photoroom.png")
+    path.join(__dirname, "../assets/license-template.png")
   );
   ctx.drawImage(template, 0, 0, width, height);
 
